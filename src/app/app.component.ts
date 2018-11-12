de6dbedd2fd85c17e7f689476a84871f9819fd9d
Creeland A. Provinsal <cree@egghead.io>
2018-11-12 14:16:45 -0800

    migrate example to codesandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,23 @@
-import { Component } from '@angular/core';
-import { WikipediaSearchService } from './wikipedia-search.service';
-import { Subject } from 'rxjs/Subject';
+import { Component } from "@angular/core";
+import { WikipediaSearchService } from "./wikipedia-search.service";
+import { Subject } from "rxjs/Subject";
+
 //application wide shared Rx operators
-import 'rxjs/add/operator/map';
+import "rxjs/add/operator/map";
 
 @Component({
   moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
-  items:Array<string>;
+  items: Array<string>;
   term$ = new Subject<string>();
-  constructor(private service:WikipediaSearchService) {
-    
-    this.term$
-        .subscribe(term => this.search(term))
+  constructor(private service: WikipediaSearchService) {
+    this.term$.subscribe(term => this.search(term));
   }
 
   search(term: string) {
-    this.service.search(term)
-                .subscribe(results => this.items = results);
+    this.service.search(term).subscribe(results => (this.items = results));
   }
 }
